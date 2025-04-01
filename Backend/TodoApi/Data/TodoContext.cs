ed8061f6b5c8df35760232cdb0b29bce24d06a12
git4roman <romanforgit@gmail.com>
2025-04-01 19:48:31 +0545

    Populating Data

--- Backend/TodoApi/Data/TodoContext.cs
+++ Backend/TodoApi/Data/TodoContext.cs
@@ -7,9 +7,18 @@ using TodoApi.Models;
 
 namespace TodoApi.Data
 {
-    public class TodoContext: DbContext
+    public class TodoContext : DbContext
     {
-        public TodoContext(DbContextOptions<TodoContext> options):base(options){}
-        public DbSet<TodoItem> Todos {get; set;}
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<TodoItem> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>().HasData(
+                new TodoItem { Id = 1, Title = "Learn ASP.NET", IsCompleted = false },
+                new TodoItem { Id = 2, Title = "Build ASP.NET", IsCompleted = false },
+                new TodoItem { Id = 3, Title = "Implement ASP.NET", IsCompleted = false }
+            );
+        }
     }
 }
---
+++ Backend/TodoApi/Migrations/20250401140305_InitialSeed.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TodoApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "IsCompleted", "Title" },
+                values: new object[,]
+                {
+                    { 1, false, "Learn ASP.NET" },
+                    { 2, false, "Build ASP.NET" },
+                    { 3, false, "Implement ASP.NET" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
