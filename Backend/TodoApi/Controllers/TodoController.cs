38708b84aa47fcb84a5d7a7be84f12fac877a5b1
git4roman <romanforgit@gmail.com>
2025-04-01 19:54:43 +0545

    Context dependency injection in Controller

---
+++ Backend/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/todos")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+
+       
+    }
+}
--- Backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ Backend/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc8b673bf9129ba3b0e941e55f887d8898dd68a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed8061f6b5c8df35760232cdb0b29bce24d06a12")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
