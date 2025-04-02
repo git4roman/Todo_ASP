fa00d57669d72fef1bb18919c1f6202b1094cb07
git4roman <romanforgit@gmail.com>
2025-04-02 12:51:23 +0545

    CRUD operations in Front End

--- TodoWeb/src/Components/TodoList.jsx
+++ TodoWeb/src/Components/TodoList.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { getTodos } from "../api";
+import { getTodos, postTodo, getTodo, editTodo, deleteTodo } from "../api";
 
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [editingTodo, setEditingTodo] = useState(null);
+  const [editingTodoText, setEditingTodoText] = useState("");
 
   useEffect(() => {
     fetchTodos();
@@ -15,6 +17,37 @@ const TodoList = () => {
     setTodos(data);
   };
 
+  const handleCreate = async () => {
+    await postTodo(newTodo);
+    setNewTodo("");
+    fetchTodos();
+  };
+
+  const handleDelete = async (id) => {
+    await deleteTodo(id);
+    fetchTodos();
+  };
+
+  const handleEdit = (todo) => {
+    setEditingTodo(todo.id);
+    setEditingTodoText(todo.title);
+    console.log("ma ya chu");
+  };
+
+  const handleUpdate = async (todo) => {
+    await editTodo(editingTodo, { ...todos, title: editingTodoText });
+    setEditingTodo(null);
+    fetchTodos();
+  };
+
+  const handleToggle = async (todo) => {
+    await editTodo(todo.id, { ...todo, isCompleted: !todo.isCompleted });
+    fetchTodos();
+  };
+  const handleCancel = () => {
+    setEditingTodo(!editTodo);
+  };
+
   return (
     <div className="todoContainer">
       <div className="inputSection">
@@ -23,18 +56,38 @@ const TodoList = () => {
           value={newTodo}
           onChange={(e) => setNewTodo(e.target.value)}
         />
-        <button>Add</button>
+        <button onClick={handleCreate}>Add</button>
       </div>
       <div className="todoList">
         <ul>
-          {todos.map((todo) => (
-            <li style={{ "list-style": "none" }}>
-              <input type="checkbox" name="todoCheck" id="todoCheck" />
-              {todo.title}
-              <button>X</button>
-              <button>Edit</button>
-            </li>
-          ))}
+          {todos.map((todo) =>
+            editingTodo != todo.id ? (
+              <li key={todo.id} style={{ "list-style": "none" }}>
+                <input
+                  type="checkbox"
+                  checked={todo.isCompleted}
+                  onChange={() => handleToggle(todo)}
+                />
+                {todo.title}
+                <button onClick={() => handleDelete(todo.id)}>X</button>
+                <button onClick={() => handleEdit(todo)}>Edit</button>
+              </li>
+            ) : (
+              <li key={todo.id}>
+                <input
+                  type="text"
+                  value={editingTodoText}
+                  onChange={(e) => setEditingTodoText(e.target.value)}
+                />
+                <button className="save" onClick={() => handleUpdate()}>
+                  Save
+                </button>
+                <button className="Cancel" onClick={() => handleCancel()}>
+                  Cancel
+                </button>
+              </li>
+            )
+          )}
         </ul>
       </div>
     </div>
--- TodoWeb/src/api.js
+++ TodoWeb/src/api.js
@@ -1,6 +1,27 @@
 import axios from "axios";
 
-export const getTodos =async ()=>{
-    const response = await axios.get("http://localhost:5138/api/todos")
-    return response.data;
-}
+const API_URL = "http://localhost:5138/api/todos";
+
+export const getTodos = async () => {
+  const response = await axios.get(`${API_URL}`);
+  return response.data;
+};
+export const postTodo = async (title) => {
+  const response = await axios.post(`${API_URL}`, {
+    title,
+    isCompleted: false,
+  });
+};
+export const getTodo = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`);
+  return response.data;
+};
+export const deleteTodo = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`);
+  //   getTodos();
+  // return response.data;
+};
+export const editTodo = async (id, todo) => {
+  const response = await axios.put(`${API_URL}/${id}`, todo);
+  return response.data;
+};
